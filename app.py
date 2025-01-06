025717a8ff27a9c895d9507b986b8a25dd21bf65
Pushpendra Baswal <89896630+Pushpendra1001@users.noreply.github.com>
2025-01-06 12:32:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def lw():
-    return "Hello There I am Pushpendra"
+    return "Hello There I am Pushpendra and I am Developer"
 
 
 app.run(host='0.0.0.0')
